b57bb855a30d71f17b55bd1565fd88918f6dd3a4
Gustavo <gustavo.ccx@hotmail.com>
2025-01-21 21:39:11 -0300

    INT-1
    - Commit de Inicialização

---
+++ src/main/java/br/com/ScreenMatchAPI/ScreenMatchApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ScreenMatchAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMatchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ScreenMatchAPI/ScreenMatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ScreenMatchAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenMatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
